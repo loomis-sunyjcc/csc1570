acbb615b8365092f5a3d32f64d3a92da853438d2
loomis-sunyjcc <loomis.sunyjcc@gmail.com>
2026-05-24 16:03:40 -0400

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,26 @@
+"""
+    lab1.py
+    Lab #1
+    CSC 1570
+
+    This program acts as a simple chatbot, mimicking
+    social interaction with the user.
+
+    Algorithm:
+    Output “Hello, my name is Ken.”
+    Output “What is your name?”
+    Store input as name
+    Output “It’s nice to meet you, name!”
+    Output “What color eyes to you have?”
+    Store input as eyeColor
+    Output “My eyes are also eyeColor, what a coincidence.”
+    Output “How old are you?”
+    Store input as age
+    Set computerAge = age + 2
+    Output “Wow, we are almost the same age.”
+    Output “I am computerAge years old.”
+    Output “I need to get a byte to eat. I’ll talk to you later.”
+"""
+__author__ = "Your Name"
+
+# Replace this comment with your code.
