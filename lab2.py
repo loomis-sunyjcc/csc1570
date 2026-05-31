b358bf11c0f7b2433955d11a0f35ebbdabcbcbf4
loomis-sunyjcc <loomis.sunyjcc@gmail.com>
2026-05-31 15:15:43 -0400

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,26 @@
+"""
+    lab2.py
+    Lab #2
+    CSC 1570
+
+    This program performs multiple complex calculations on
+
+    Output “Please enter a decimal number: ”.
+    Store input as decNum.
+
+    Set floorNum = the floor of decNum.
+    Set ceilNum = the ceiling of the decNum.
+    Set wholePosNum = absolute value of decNum rounded to the nearest integer.
+    Set sqrNum = calculate the square of wholePosNum.
+    Set cubeNum = calculate the cube of wholePosNum.
+    Set sqrRootNum = calculate the square root of wholePosNum.
+
+    Output “The floor of decNum is floorNum”
+    Output “The ceiling of decNum is ceilNum”
+    Output “The square of wholePosNum is sqrNum”
+    Output “The cube of wholePosNum is cubeNum”
+    Output “The square root of wholePosNum is sqrRootNum”
+
+"""
+__author__ = "Your name"
+
